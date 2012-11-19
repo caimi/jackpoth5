5cb93956bfee8315e6eb880d42492f5618d7c9aa
Carlos Caimi <carlos.caimi@gmail.com>
2012-11-19 11:20:33 -0200

    jump in time

--- game.js
+++ game.js
@@ -1,31 +1,92 @@
-var W = 1280;
-var H = 800;
-var prize = 1;
-var READY = 0;
-var RUNNING = 1;
-var DYING = 2
+var UJAPP = {};
+UJAPP.W = 1280;
+UJAPP.H = 800;
+UJAPP.prize = 1;
+UJAPP.READY = 0;
+UJAPP.RUNNING = 1;
+UJAPP.DYING = 2;
+UJAPP.PAUSE = true;
+UJAPP.vmin = -5.0;
+UJAPP.vmax = 5.0;
 
-window.onload = function () {
-    //start crafty
-    Crafty.init(W, H);
+window.onload = function() {
+	//start crafty
+	Crafty.init(UJAPP.W, UJAPP.H);
+	Crafty.viewport.init();
 	Crafty.scene("loading");
-	Crafty.pause();
-    //Crafty.canvas.init();
+	//Crafty.pause();
+	//Crafty.canvas.init();
 };
 
-//the loading screen that will display while our assets load
-Crafty.scene("loading", function () {
-    //load takes an array of assets and a callback when complete
-	Crafty.e("2D, DOM, Text").attr({ w: 100, h: 20, x: 150, y: 120 })
-		.text("Loading")
-		.css({ "text-align": "center" });
+Crafty.scene("loading", function() {
 
-		//"buttons.png", "b_branca.png", "b_preta.png", "explosions.png", "explosions2.png", "jackpot_1024x768.png", "jackpot_1280x1024.png", "jackpot_1280x800.png", "jackpot_800x600.png","start.png"
-    Crafty.load(["ball.png", "jackpot_1440x900.png"], function () {
-        Crafty.scene("main"); //when everything is loaded, run the main scene
-    });
-		Crafty.sprite(23, 26, "ball.png", {
+	Crafty.load(["jackpot_1280x800.png", "jackpot_1280x1024.png", "splash.png", "ball2.png", "ball.png"], function() {
+		setTimeout(function() {
+			Crafty.scene("splash");
+			// Play the main scene
+		}, 100);
+	});
+	
+	Crafty.audio.add("hit", "hit.wav");
+	
+	Crafty.sprite(74, 74, "start.png", {
+		bt_start:  [0,0],
+		bt_start_over:  [1,0],
+		bt_start_clicked:  [2,1],
+		bt_start_on:  [3,1]
+	});
+	
+	Crafty.sprite(34, 34, "buttons.png", {
+		bt_pause:  [0,0],
+		bt_pause_over:  [1,0],
+		bt_cueball:  [0,1],
+		bt_cueball_over:  [1,1],
+		bt_sound:  [0,2],
+		bt_sound_over:  [1,2],
+		bt_random: [0,3],
+		bt_random_over: [1,3],
+		bt_label: [0,4],
+		bt_label_over: [1,4],
+		bt_add:[0,5],
+		bt_add_over:[1,5],
+		bt_remove:[0,6],
+		bt_remove_over:[1,6],
+		bt_menu:[0,7],
+		bt_menu_over:[1,7]
+	});
+	
+	Crafty.sprite(26, 26, "ball2.png", {
 		ball1:  [0,0],
+		ball2:  [0,1],
+		ball3:  [0,2],
+		ball4:  [0,3],
+		ball5:  [0,4],
+		ball6:  [0,5],
+		ball7:  [1,0],
+		ball8:  [1,1],
+		ball9:  [1,2],
+		ball10: [1,3],
+		ball11: [1,4],
+		ball12: [1,5],
+		ball13: [2,0],
+		ball14: [2,1],
+		ball15: [2,2],
+		ball16: [2,3],
+		ball17: [2,4],
+		ball18: [2,5],
+		ball19: [3,0],
+		ball20: [3,1],
+		ball21: [3,2],
+		ball22: [3,3],
+		ball23: [3,4],
+		ball24: [3,5],
+		ball25: [4,0],
+		ball26: [4,1],
+		ball27: [4,2],
+		ball28: [4,3],
+		ball29: [4,4],
+		ball30: [4,5],
+		ball31: [5,0]		/*ball1:  [0,0],
 		ball2:  [1,0],
 		ball3:  [2,0],
 		ball4:  [3,0],
@@ -40,96 +101,160 @@ Crafty.scene("loading", function () {
 		ball13: [0,3],
 		ball14: [1,3],
 		ball15: [2,3],
-		ball16: [3,3]
+		ball16: [3,3]*/
 	});
 });
 
-Crafty.scene("main", function () {
-	var font_style = { "font-family": "impact", "font-size": "30px", "text-align":"right"};
-	var img = "jackpot_"+Crafty.viewport.width+"x"+Crafty.viewport.height;
-	var bg = Crafty.e("2D, DOM, Image")
-             .attr({w: Crafty.viewport.width, h: Crafty.viewport.height})
-             .image(img+".png", "no-repeat");  
-    Crafty.background(bg);
+Crafty.scene("splash", function() {
+	Crafty.viewport.scale(900/500);
+	var bg = Crafty.e("2D, DOM, Image, Mouse")
+		.image("splash_800x500.png", "no-repeat")
+		.bind('Click', function(){ Crafty.scene("jackpot") });  
+	Crafty.background(bg);
+});
 
-	var players = 50;
-	for (var i = 0; i < players; i++) {
-		axisX = Crafty.math.randomInt(200, W-43);
-		axisY = Crafty.math.randomInt(70, H-123);
+Crafty.scene("jackpot", function() {
+	Crafty.viewport.scale(1.125);
+	var img = "jackpot_1280x800";// + Crafty.viewport.width + "x" + Crafty.viewport.height;
+	var bg = Crafty.e("2D, DOM, Image").attr({
+		w : Crafty.viewport.width,
+		h : Crafty.viewport.height
+	}).image(img + ".png", "no-repeat");
+	Crafty.background(bg);
+	Crafty.e("HTML, Keyboard")
+	   .attr({x:20, y:70, w:180, h:UJAPP.H-10})
+	   .replace("<textarea style='width: 170px;height: 620px; background-color: #000;'></textarea>");
+	Crafty.e("2D, DOM, Text, cueBallTotal")
+		.attr({ x: 340, y: UJAPP.H-60, w: 40, h: 30 })
+		.css({"font-family":"impact", "font-size":"24pt", "Color":"#fff", "text-align":"right"})
+	// Crafty.e("2D, DOM, bt_pause, Button")
+		// .attr({ x : 600, y : UJAPP.H-90, w : 34, h : 34 })
+	Crafty.e("2D, DOM, bt_cueball, Button")
+		.attr( { x : 640, y : UJAPP.H-90, w : 34, h : 34} )
+		.bind("Click", function()
+			{
+				Crafty.e("Ball, ball31").attr({ x:700, y:400});
+				Crafty("cueBallTotal").text(Crafty("ball31").length);
+			}
+		)
+		.line(1);
+	Crafty.e("2D, DOM, bt_sound, Button")
+		.attr( { x : 680, y : UJAPP.H-90, w : 34, h : 34} )
+		.line(2);
+	Crafty.e("2D, DOM, bt_random, Button")
+		.attr( { x : 720, y : UJAPP.H-90, w : 34, h : 34} )
+		.bind("Click", randomVelocity)
+		.line(3);
+	Crafty.e("2D, DOM, bt_label, Button")
+		.attr( { x : 760, y : UJAPP.H-90, w : 34, h : 34} )
+		.line(4);
+	Crafty.e("2D, DOM, bt_add, Button")
+		.attr( { x : 800, y : UJAPP.H-90, w : 34, h : 34} )
+		.line(5);	
+	Crafty.e("2D, DOM, bt_remove, Button")
+		.attr( { x : 840, y : UJAPP.H-90, w : 34, h : 34} )
+		.line(6);
+	Crafty.e("2D, DOM, bt_menu, Button")
+		.attr( { x : 880, y : UJAPP.H-90, w : 34, h : 34} )
+		.bind("Click", function(){ Crafty.scene("splash");})
+		.line(7);
 		
-		Crafty.e("2D, DOM, ball" + Crafty.math.randomInt(1, 16) + ", Collision")
-			.attr({ x: axisX, y: axisY, z:1, dX: Crafty.math.randomInt(-5.0, 5.0), dY: Crafty.math.randomInt(-5.0, 5.0) })
-			.bind('EnterFrame', detectCollision)
-			.bind('KeyDown', function(e){
-				if(e.key == Crafty.keys['PAGE_UP']) 
-			     	this.dX = this.dX + 1;
-			    else if(e.key == Crafty.keys['PAGE_DOWN'])
-			     	this.dX = this.dX + 1;
-				else if(e.key == Crafty.keys['SPACE']){
-			     	this.dX = Crafty.math.randomInt(-250/Crafty.timer.getFPS(), 250/Crafty.timer.getFPS());
-			     	this.dY = Crafty.math.randomInt(-250/Crafty.timer.getFPS(), 250/Crafty.timer.getFPS());					
-				}
-			})
+	Crafty.e("2D, DOM, bt_start, Button")
+		.attr( { x : 520, y : UJAPP.H-90, w : 74, h : 74} )
+		.bind("Click", function(){ pause()})
+		.setModeOnOff(true);
+		
+	players = 30;
+	for (var i = 0; i < players; i++){
+		b = Crafty.e("Ball, ball" + ((i % 31)+1));
 	}
 	
-	Crafty.e("2D, DOM, Text").attr({ w: 210, h: 60, x: 210, y: 45 })
-		.bind('EnterFrame', function(){
-			this.text('fps: '+Crafty.timer.getFPS())
-		})
-		.css({ "font-family": "tahoma", "font-size": "12px", "color":"#a1a1a1"});
-	Crafty.e("2D, DOM, Text").attr({ w: 100, h: 30, x: 0, y: H-60 })
-		.text(players)
-		.css(font_style);
-	Crafty.e("2D, DOM, Text").attr({ w: 200, h: 30, x: 0, y: H-60 })
-		.text(players)
-		.css(font_style);
-	Crafty.e("2D, DOM, Text").attr({ w: 305, h: 30, x: 0, y: H-60 })
-		.text("10")
-		.css(font_style);
-	Crafty.e("2D, DOM, Text").attr({ w: 380, h: 30, x: 0, y: H-60 })
-		.text("0")
-		.css(font_style);
-	Crafty.e("2D, DOM, Text").attr({ w: 463, h: 30, x: 0, y: H-60 })
-		.text(prize)
-		.css(font_style)
-		.bind('KeyDown', function(e){
-			if(e.key == Crafty.keys['ENTER']) 
-		     	Crafty.pause()
-		     });	
+	Crafty("cueBallTotal").text(Crafty("ball31").length);
 });
 
-function createPlayer(name, c){
-	var player = new Object();
-	
-	player.status = READY;
-	player.color = c;
-	player.x = Crafty.math.randomInt(200, W-43);
-	player.y = Crafty.math.randomInt(70, H-123);
-	player.dx = Crafty.math.randomInt(-5, 5);
-	player.dy = Crafty.math.randomInt(-5, 5);
-	player.name = name;
-	
-	return player;
-}
+Crafty.c("Button",{
+	_sound: 'hit',
+	_line: 0,
+	_onOff: false,
+	_state: false,
+	init: function(){
+		this.addComponent("2D, Mouse");
+		this.bind('Click', function(){
+			Crafty.audio.play(this._sound);
+			if(this._onOff){
+				if(this._state)
+					this.sprite(0, this._line);
+				else
+					this.sprite(3, this._line);	 
+				this._state = !this._state;
+			}
+		});
+		this.bind('MouseOver', function(){ if(!this._state) this.sprite(1, this._line); })
+		this.bind('MouseOut', function(){ if(!this._state) this.sprite(0, this._line); })
+	},
+	line: function(l) { this._line = l;},
+	soundFile: function(sf) { this._sound = sf; },
+	setModeOnOff: function(b) { this._onOff = b}
+});
+
+Crafty.c("Ball",{
+	_state: UJAPP.READY,
+	init: function()
+	{
+		this.addComponent("2D, DOM, Collision");
+		this.attr({ x: Crafty.math.randomInt(200, UJAPP.W-43), y: Crafty.math.randomInt(70, UJAPP.H-123), z:1, dX: Crafty.math.randomInt(UJAPP.vmin, UJAPP.vmax), dY: Crafty.math.randomInt(UJAPP.vmin, UJAPP.vmax) });
+		this.bind('EnterFrame', detectCollision)
+		this.bind('KeyDown', function(e)
+		{
+			if(e.key == Crafty.keys['PAGE_UP']) 
+		     	this.dX = this.dX + 1;
+		    else if(e.key == Crafty.keys['PAGE_DOWN'])
+		     	this.dX = this.dX + 1;
+			else if(e.key == Crafty.keys['SPACE']){
+		     	this.dX = Crafty.math.randomInt(UJAPP.vmin, UJAPP.vmax);
+		     	this.dY = Crafty.math.randomInt(UJAPP.vmin, UJAPP.vmax);					
+			};
+		});
+	}
+});
 
 function detectCollision(){
+	if(UJAPP.PAUSE) return;
 	if(this.x < 200){		
 		this.dX = -this.dX;
 		this.x = 200;
 	}
-	if(this.x > W-43){
+	if(this.x > UJAPP.W-43){
 		this.dX = -this.dX;
-		this.x = W-43;	
+		this.x = UJAPP.W-43;	
 	}
 	if(this.y < 70){
 		this.dY = -this.dY;
 		this.y = 70;		
 	}
-	if(this.y > H-123){
+	if(this.y > UJAPP.H-123){
 		this.dY = -this.dY;
-		this.y = H-123;
+		this.y = UJAPP.H-123;
 	}
 	
 	this.x += this.dX;
 	this.y += this.dY;
+}
+
+function isCollided(b1, b2){
+	return false;
+}
+
+function pause(){
+	UJAPP.PAUSE = !UJAPP.PAUSE;
+}
+
+function randomVelocity(){
+	var ent = Crafty("Collision");
+	for(i=0; i<ent.length; i++){
+		Crafty(ent[i]).attr({
+			dX: Crafty.math.randomInt(UJAPP.vmin, UJAPP.vmax),
+			dY: Crafty.math.randomInt(UJAPP.vmin, UJAPP.vmax)
+		})
+	}
 }
